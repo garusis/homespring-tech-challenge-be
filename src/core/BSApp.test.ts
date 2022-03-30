3c319e4055dcfdd788528b80bce0391d84299c42
Marcos Alvarez <garusis@gmail.com>
2022-03-30 00:52:52 -0500

    chores: Update test cases for BSApp

--- src/core/BSApp.test.ts
+++ src/core/BSApp.test.ts
@@ -1,5 +1,36 @@
+import { Container, Service } from 'typedi';
+import request from 'supertest';
+import BSApp from './BSApp';
+
+@Service()
+class MockController {
+  private static internalInstancesCount = 0;
+
+  static get instancesCount() {
+    return this.internalInstancesCount;
+  }
+
+  constructor() {
+    MockController.internalInstancesCount += 1;
+  }
+}
+
 describe('BSApp', () => {
+  let bsApp: BSApp;
+
+  beforeAll(() => {
+    bsApp = Container.get(BSApp);
+  });
+
+  it('Setups the API with a default health check', async () => {
+    await request(bsApp.app).get('/health').expect(200);
+  });
+
   it('Loads a provided list of controllers', () => {
-    void 0;
+    expect(MockController.instancesCount).toBe(0);
+
+    bsApp.setupControllers([MockController]);
+
+    expect(MockController.instancesCount).toBe(1);
   });
 });
