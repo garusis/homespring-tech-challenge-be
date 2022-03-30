c78aaeb401884f0d2a3ac1b1527421a96d34c7a6
Marcos Alvarez <garusis@gmail.com>
2022-03-30 05:01:14 -0500

    feat: Add @ValidateQueryParams decorators

--- src/core/decorators/ValidateQueryParams.ts
+++ src/core/decorators/ValidateQueryParams.ts
@@ -1,9 +1,29 @@
 import { ObjectSchema } from 'joi';
+import { Request, Response } from 'express';
 
 export default function ValidateQueryParams(
   schema: ObjectSchema,
 ): MethodDecorator {
-  return function (target, propertyKey, descriptor) {
-    void 0;
+  return function (target, propertyKey, descriptor: PropertyDescriptor) {
+    const previousHandler = descriptor.value;
+
+    // eslint-disable-next-line no-param-reassign
+    descriptor.value = (req: Request, res: Response) => {
+      const { value, error } = schema.validate(req.query, {
+        abortEarly: false,
+        stripUnknown: true,
+      });
+
+      if (error) {
+        res.status(400).json(error.details);
+        return;
+      }
+
+      req.query = value;
+
+      if (typeof descriptor.value === 'function') {
+        previousHandler(req, res);
+      }
+    };
   };
 }
