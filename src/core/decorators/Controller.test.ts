ff8d104289d6cd3a9e086dca81dff717117aecf6
Marcos Alvarez <garusis@gmail.com>
2022-03-30 03:11:42 -0500

    chores: Update test cases for @Controller decorator

--- src/core/decorators/Controller.test.ts
+++ src/core/decorators/Controller.test.ts
@@ -1,13 +1,26 @@
-import { Service, Container } from 'typedi';
-import Controller, { getControllerPathKey } from './Controller';
+import { Container } from 'typedi';
+import { Request, Response } from 'express';
+import request from 'supertest';
+import Controller from './Controller';
+import Endpoint from './Endpoint';
+import BSApp from '../BSApp';
 
 @Controller('test-path')
-@Service()
-class MockController {}
+class MockController {
+  @Endpoint({ path: 'test', method: 'get' })
+  method(req: Request, res: Response) {
+    res.sendStatus(200);
+  }
+}
 
 describe('Controller', () => {
-  it('Attaches the controller path to the controller instance', () => {
-    const instance = Container.get(MockController);
-    expect(getControllerPathKey(instance)).toBe('test-path');
+  let bsApp: BSApp;
+
+  beforeAll(() => {
+    bsApp = Container.get(BSApp);
+  });
+
+  it('Attaches a new router to the app based on the controller path and attaches its endpoints', async () => {
+    await request(bsApp.app).get('/test-path/test').expect(200);
   });
 });
